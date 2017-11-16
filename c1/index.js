eb9593dd8d115c93d39ec81af80d529cf67f5d7b
Vesna Janakievska-Bugarski <32159101+vesnajb@users.noreply.github.com>
2017-11-16 13:32:01 +0100

    cas1 java

---
+++ c1/index.js
@@ -0,0 +1,18 @@
+console.log('Hello World');
+
+var ime = "Vesna"; // string (niza od karakteri)
+var godini = 34; // number
+var visina = 168.3; // number
+var temperatura = -22.5; // number
+var classEnded = true; // boolean
+
+console.log(ime);
+console.log(godini);
+console.log(visina);
+console.log(temperatura);
+console.log(classEnded);
+
+godinaNaRagjanje = 1983; // camel case style
+godina_na_ragjanje = 1983; // snake case
+godinanaragjanje = 1983; // BAD CASE!
+
