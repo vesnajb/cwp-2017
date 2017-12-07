3c8a7edcb8dde90c64528149161dfff984ea4855
Vesna Janakievska-Bugarski <32159101+vesnajb@users.noreply.github.com>
2017-12-07 10:02:00 +0100

    popravena domasna 2

--- Domasna 2/index.js
+++ Domasna 2/index.js
@@ -3,63 +3,56 @@ var niza2 = [5, 7, 13, 4, 8, 14, 13, 25, 11, 16, 42, 9, 12, 44, 26, 22, 34];
 var niza3 = [3, 8, 6, 92, 34, 51, 36, 13, 17, 14, 11, 24, 57, 58,102, 108];
 
 function sekojTretParenBroj(niza){
-	var b=0;
-	for(var i=0; i< niza.length; i++){
-		if (niza[i]%2==0) {
-				b=b+1;
-				if (b==3){ 
-					console.log(niza[i]);
-					b=0;
-				};
+	var b = 0;
+	for(var i = 0; i < niza.length; i++){
+		if (niza[i] % 2 == 0) {
+			b = b + 1;
+			if (b == 3){ 
+				console.log(niza[i]);
+				b = 0;
+			};
 		}
 	}
 }
+
 sekojTretParenBroj(niza1);
 sekojTretParenBroj(niza2);
 sekojTretParenBroj(niza3);
 
 
-var sum=0;
-
 function zbirNaNeparniBroevi(niza){
-	sum=0;
-	for(var i=0; i< niza.length; i++){
-		if (niza[i]%2==1) {
-			sum=sum+niza[i];
-		};
-};
-return "sum";
-};
+	var sum = 0;
+	for(var i = 0; i < niza.length; i++){
+		if (niza[i] % 2 == 1) {
+			// sum = sum + niza[i];
+			sum += niza[i];
+		}
+	}
 
-zbirNaNeparniBroevi(niza1);
-console.log(sum);
-zbirNaNeparniBroevi(niza2);
-console.log(sum);
-zbirNaNeparniBroevi(niza3);
-console.log(sum);
-
-
-var p=0;
-function prosek(niza){
-	p=0;
-	for(var i=0; i<niza.length; i++){
-		p=p+niza[i];
-	};
-	p=p/niza.length;
-	return 'p';
+	return sum;
 }
-prosek(niza1);
-console.log(p);
-prosek(niza2);
-console.log(p);
-prosek(niza3);
-console.log(p);
+
+console.log(zbirNaNeparniBroevi(niza1));
+
+console.log(zbirNaNeparniBroevi(niza2));
+
+console.log(zbirNaNeparniBroevi(niza3));
 
 
 
-// zbirNaNeparniBroevi(niza5);
-// // 21
 
-// prosek(niza5);
-// //47 / 16 = 2.9375
+function prosek(niza){
+	var p = 0;
+	for(var i = 0; i < niza.length; i++){
+		p = p + niza[i];
+	}
 
+	p = p / niza.length;
+	return p;
+}
+
+console.log(prosek(niza1));
+
+console.log(prosek(niza2));
+
+console.log(prosek(niza3));
